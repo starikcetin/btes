8195ea0fcd843b936b2de29f306101739990e14d
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2020-12-17 10:12:35 +0300

    eslint config improvements

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,33 +1,35 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true
-    },
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
         project: './tsconfig.json',
         tsconfigRootDir: __dirname,
+      },
     },
-    plugins: [
-        "@typescript-eslint",
-        "prettier",
-    ],
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "react-app",
-        "prettier",
-        "prettier/@typescript-eslint",
-        "prettier/babel",
-        "prettier/flowtype",
-        "prettier/prettier",
-        "prettier/react",
-        "prettier/standard",
-        "prettier/unicorn",
-        "prettier/vue",
-    ],
-    rules: {
-        "prettier/prettier": "error",
-    }
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'react-app',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/babel',
+    'prettier/flowtype',
+    'prettier/prettier',
+    'prettier/react',
+    'prettier/standard',
+    'prettier/unicorn',
+    'prettier/vue',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
