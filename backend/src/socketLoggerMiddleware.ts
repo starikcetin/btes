fd3524fcd597165b6395d9b368ea4a0076fb285b
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2020-12-22 06:50:25 +0300

    backend: improvements on logger middleware
    added an environment variable to determine the log level of the socket logger middleware

--- backend/src/socketLoggerMiddleware.ts
+++ backend/src/socketLoggerMiddleware.ts
@@ -3,26 +3,33 @@
 
 import { Socket } from 'socket.io';
 
+const SocketLogLevels = ['none', 'summary', 'full'] as const;
+type SocketLogLevel = typeof SocketLogLevels[number];
+
+const socketLogLevel: SocketLogLevel = resolveLogLevel();
+
 export const socketLoggerMiddleware = (
   socket: Socket,
   next: (err?: never) => void
 ): void => {
-  const realSocketEmit = socket.emit.bind(socket);
-  const realSocketOn = socket.on.bind(socket);
-  const realNspEmit = socket.nsp.emit.bind(socket.nsp);
-  const realNspOn = socket.nsp.on.bind(socket.nsp);
+  if (socketLogLevel !== 'none') {
+    const realSocketEmit = socket.emit.bind(socket);
+    const realSocketOn = socket.on.bind(socket);
+    const realNspEmit = socket.nsp.emit.bind(socket.nsp);
+    const realNspOn = socket.nsp.on.bind(socket.nsp);
 
-  socket.emit = (event, ...args) =>
-    wrapEmit(event, socket, args, realSocketEmit);
+    socket.emit = (event, ...args) =>
+      wrapEmit(event, socket, args, realSocketEmit);
 
-  socket.on = (event, listener) =>
-    wrapOn(socket, event, listener, realSocketOn);
+    socket.on = (event, listener) =>
+      wrapOn(socket, event, listener, realSocketOn);
 
-  socket.nsp.emit = (event, ...args) =>
-    wrapEmit(event, socket, args, realNspEmit);
+    socket.nsp.emit = (event, ...args) =>
+      wrapEmit(event, socket, args, realNspEmit);
 
-  socket.nsp.on = (event: string, listener: Function) =>
-    wrapOn(socket, event, listener, realNspOn);
+    socket.nsp.on = (event: string, listener: Function) =>
+      wrapOn(socket, event, listener, realNspOn);
+  }
 
   next();
 };
@@ -42,15 +49,11 @@ function wrapEmit(
 
     case 'connect':
     case 'disconnect':
-      console.log(`[socket --> ${socket.id} on ${socket.nsp.name}]`, event);
+      log([`[socket --> ${socket.id}]`, event]);
       break;
 
     default:
-      console.log(
-        `[socket --> ${socket.id} on ${socket.nsp.name}]`,
-        event,
-        ...args
-      );
+      log([`[socket --> ${socket.id}]`, event], [...args]);
       break;
   }
 
@@ -82,18 +85,33 @@ function makeWrappedListener(
 
       case 'connect':
       case 'disconnect':
-        console.log(`[socket <-- ${socket.id} on ${socket.nsp.name}]`, event);
+        log([`[socket <-- ${socket.id}]`, event]);
         break;
 
       default:
-        console.log(
-          `[socket <-- ${socket.id} on ${socket.nsp.name}]`,
-          event,
-          ...listenerArgs
-        );
+        log([`[socket <-- ${socket.id}]`, event], [...listenerArgs]);
         break;
     }
 
     return listener(...listenerArgs);
   };
+}
+
+function log(summary: any[], full: any[] = []) {
+  if (socketLogLevel === 'full') {
+    console.log(...summary, ...full);
+  } else if (socketLogLevel === 'summary') {
+    console.log(...summary);
+  }
+}
+
+function resolveLogLevel(): SocketLogLevel {
+  const logLevelRaw = process.env.SOCKET_LOG_LEVEL;
+
+  if (logLevelRaw && !SocketLogLevels.some((s) => logLevelRaw === s)) {
+    const valid = SocketLogLevels.join(', ');
+    throw `SOCKET_LOG_LEVEL environment variable is invalid. Given: ${logLevelRaw} | Valid: ${valid}`;
+  }
+
+  return (process.env.SOCKET_LOG_LEVEL || 'summary') as SocketLogLevel;
 }
