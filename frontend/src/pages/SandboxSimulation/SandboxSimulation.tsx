452889310ba2a4d40e1ef373b3a272c029925bd5
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2020-12-27 07:36:19 +0300

    removed simulation ping section from the bottom of the simulation page & added a context menu item for sending pings

--- frontend/src/pages/SandboxSimulation/SandboxSimulation.tsx
+++ frontend/src/pages/SandboxSimulation/SandboxSimulation.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { ContextMenu, ContextMenuTrigger, MenuItem } from 'react-contextmenu';
 
 import './SandboxSimulation.scss';
@@ -15,12 +15,7 @@ interface SandboxSimulationParamTypes {
 
 const SandboxSimulation: React.FC = () => {
   const [connected, setConnected] = useState(false);
-  const simulationPongTextAreaRef = useRef<HTMLTextAreaElement>(null);
   const { simulationUid } = useParams<SandboxSimulationParamTypes>();
-
-  const simulationPongs = useSelector(
-    (state: RootState) => state.simulation[simulationUid]?.pongs || []
-  );
 
   const nodes = useSelector((state: RootState) =>
     Object.values(state.simulation[simulationUid]?.nodeMap || {})
@@ -39,19 +34,6 @@ const SandboxSimulation: React.FC = () => {
 
   const sendSimulationPingOnClick = () => {
     simulationBridge.sendSimulationPing(simulationUid, { date: Date.now() });
-  };
-
-  const formatSimulationPongs = () => {
-    const result = simulationPongs
-      ?.map(
-        (simPong) =>
-          `ping: ${new Date(simPong.pingDate).toUTCString()}\t` +
-          `pong: ${new Date(simPong.pongDate).toUTCString()}`
-      )
-      .reverse()
-      .join('\n');
-
-    return result || '';
   };
 
   const createNode = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
@@ -75,7 +57,7 @@ const SandboxSimulation: React.FC = () => {
         <>
           <div className="row">
             <ContextMenuTrigger id="rightClickArea" holdToDisplay={-1}>
-              <div className="d-flex position-absolute h-75 border w-100">
+              <div className="d-flex position-absolute h-100 border w-100">
                 {nodes.map((node) => (
                   <SimulationNode
                     key={node.nodeUid}
@@ -88,26 +70,8 @@ const SandboxSimulation: React.FC = () => {
             </ContextMenuTrigger>
             <ContextMenu id="rightClickArea">
               <MenuItem onClick={createNode}>Create Node</MenuItem>
+              <MenuItem onClick={sendSimulationPingOnClick}>Send Ping</MenuItem>
             </ContextMenu>
-          </div>
-          <div className="fixed-bottom d-flex row align-content-center m-3">
-            <div className="input-group">
-              <div className="input-group-append">
-                <input
-                  type="button"
-                  className="form-control btn btn-outline-success"
-                  onClick={sendSimulationPingOnClick}
-                  value="Send Simulation Ping"
-                />
-              </div>
-              <textarea
-                className="form-control"
-                style={{ resize: 'both' }}
-                readOnly
-                ref={simulationPongTextAreaRef}
-                value={formatSimulationPongs()}
-              ></textarea>
-            </div>
           </div>
           <NodeModal
             closeHandler={() => setViewingNodeUid(null)}
