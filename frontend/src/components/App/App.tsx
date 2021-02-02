0b1340fea4ce253d3890ffd7537d26e64a78ebdd
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2021-02-03 01:25:08 +0300

    document containers, Sandbox and Home pages done
    Completed:
    * Document containers (html, body, #root, App)
    * Sandbox & Home
    
    To Do:
    * SandboxSimulation (make it fixed size for now)
    cleanup: remove about page long debug lorem ipsum text

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -9,37 +9,41 @@ import SandboxSimulation from '../../pages/SandboxSimulation/SandboxSimulation';
 
 const App: React.FC = () => {
   return (
-    <div className="comp-app">
-      <Navbar></Navbar>
-      <Switch>
-        <Route path="/sandbox">
-          <Sandbox></Sandbox>
-        </Route>
-        <Route path="/lessons">
-          {/* <Lessons></Lessons> */}
-          <div>
-            <p>Lessons</p>
-          </div>
-        </Route>
-        <Route path="/explorer">
-          {/* <Explorer></Explorer> */}
-          <div>
-            <p>Explorer</p>
-          </div>
-        </Route>
-        <Route path="/about">
-          {/* <About></About> */}
-          <div>
-            <p>About</p>
-          </div>
-        </Route>
-        <Route path="/sandboxSimulation/:simulationUid">
-          <SandboxSimulation></SandboxSimulation>
-        </Route>
-        <Route path="/">
-          <Home></Home>
-        </Route>
-      </Switch>
+    <div className="global-document-container comp-app">
+      <div className="comp-app--header-container">
+        <Navbar></Navbar>
+      </div>
+      <div className="comp-app--page-container">
+        <Switch>
+          <Route path="/sandbox">
+            <Sandbox></Sandbox>
+          </Route>
+          <Route path="/lessons">
+            {/* <Lessons></Lessons> */}
+            <div>
+              <p>Lessons</p>
+            </div>
+          </Route>
+          <Route path="/explorer">
+            {/* <Explorer></Explorer> */}
+            <div>
+              <p>Explorer</p>
+            </div>
+          </Route>
+          <Route path="/about">
+            {/* <About></About> */}
+            <div>
+              <p>About</p>
+            </div>
+          </Route>
+          <Route path="/sandboxSimulation/:simulationUid">
+            <SandboxSimulation></SandboxSimulation>
+          </Route>
+          <Route path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 };
