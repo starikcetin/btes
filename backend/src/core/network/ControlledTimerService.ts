eedcceeb0bed2dfa522aca2edb0a4c3b6c3c2cd0
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2021-02-18 02:43:09 +0300

    backend: created ControlledTimer and ControlledTimerService

---
+++ backend/src/core/network/ControlledTimerService.ts
@@ -0,0 +1,65 @@
+import _ from 'lodash';
+import { ControlledTimer } from './ControlledTimer';
+import { ControlledTimerConfig } from './ControlledTimerConfig';
+
+export class ControlledTimerService {
+  private readonly activeTimers: ControlledTimer[] = [];
+  private readonly rawTickPeriodInMs = 10;
+
+  private currentTicker?: NodeJS.Timeout;
+  private timeScale = 1;
+
+  private get scaledTickPeriodInMs() {
+    return this.rawTickPeriodInMs * this.timeScale;
+  }
+
+  /** Starts the service. Time won't flow before you call this. */
+  public readonly begin = (): void => {
+    this.currentTicker = setInterval(this.tick, this.rawTickPeriodInMs);
+  };
+
+  public readonly pause = (): void => {
+    if (undefined !== this.currentTicker) {
+      clearInterval(this.currentTicker);
+    }
+  };
+
+  public readonly resume = (): void => {
+    this.currentTicker = setInterval(this.tick, this.rawTickPeriodInMs);
+  };
+
+  /**
+   * Changes how fast the time is flowing. A larger time scale means active timers will finish faster.
+   *
+   * Time scale is initially `1`.
+   *
+   * At each tick, active timers will be advanced using this formula: `elapsedTime = timeScale * tickPeriod`
+   */
+  public readonly setTimeScale = (timeScale: number): void => {
+    this.timeScale = timeScale;
+  };
+
+  public readonly createTimer = (
+    config: ControlledTimerConfig
+  ): ControlledTimer => {
+    const newTimer = new ControlledTimer(config, this.cleanupTimer);
+    this.activeTimers.push();
+    return newTimer;
+  };
+
+  private readonly tick = (): void => {
+    const scaledTickPeriodInMs = this.scaledTickPeriodInMs;
+
+    for (const timer of this.activeTimers) {
+      timer.advance(scaledTickPeriodInMs);
+
+      if (timer.isDone) {
+        this.cleanupTimer(timer);
+      }
+    }
+  };
+
+  private readonly cleanupTimer = (timer: ControlledTimer) => {
+    _.remove(this.activeTimers, timer);
+  };
+}
