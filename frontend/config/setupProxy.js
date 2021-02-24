f2ff556a38074da57e0dab93a15dc0fbfb00cb78
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2021-02-25 00:27:47 +0300

    devops (frontend): added an HPM log level environment variable
    hush HPM

--- frontend/config/setupProxy.js
+++ frontend/config/setupProxy.js
@@ -1,11 +1,15 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const logLevel = process.env.REACT_APP_HPM_LOG_LEVEL || 'warn';
 
 module.exports = (app) => {
-  const restApiBaseUrl = "/api/rest";
-  const socketApiBaseUrl = "/api/socket";
+  const restApiBaseUrl = '/api/rest';
+  const socketApiBaseUrl = '/api/socket';
 
-  const restApiProxyAddress = process.env.REACT_APP_REST_PROXY || "http://localhost:3001";
-  const socketApiProxyAddress = process.env.REACT_APP_SOCKET_PROXY || "http://localhost:3002";
+  const restApiProxyAddress =
+    process.env.REACT_APP_REST_PROXY || 'http://localhost:3001';
+  const socketApiProxyAddress =
+    process.env.REACT_APP_SOCKET_PROXY || 'http://localhost:3002';
 
   registerApiProxy(app, restApiBaseUrl, restApiProxyAddress, false);
   registerApiProxy(app, socketApiBaseUrl, socketApiProxyAddress, true);
@@ -13,12 +17,12 @@ module.exports = (app) => {
 
 /**
  * `baseUrl/foo` gets proxied to `proxyAddress/foo`
- * 
+ *
  * example:
  * ```txts
  * baseUrl: /bar/baz
  * proxyAddress: http://test.com
- * 
+ *
  * /bar/baz/foo -> http://test.com/foo
  * ```
  */
@@ -26,9 +30,9 @@ function registerApiProxy(app, baseUrl, proxyAddress, handleSockets) {
   app.use(
     createProxyMiddleware(baseUrl, {
       target: proxyAddress,
-      pathRewrite: { [`^${baseUrl}`]: "" },
+      pathRewrite: { [`^${baseUrl}`]: '' },
       ws: handleSockets,
-      logLevel: "debug",
+      logLevel,
     })
   );
 }
