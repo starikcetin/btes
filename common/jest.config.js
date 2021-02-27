23ddb93e1290a4eb83431fb2d000dcd94e0b276e
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2021-02-27 14:17:07 +0300

    common package jest setup

---
+++ common/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.ts',
+    '<rootDir>/src/**/*.spec.ts',
+  ],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+};
