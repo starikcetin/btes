f69ef822b774a1b55807b204cd71f0881480d5f5
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2021-03-16 05:18:56 +0300

    wip on frontend wallet

--- frontend/src/components/blockchain/BlockchainWalletPane/BlockchainWalletPane.tsx
+++ frontend/src/components/blockchain/BlockchainWalletPane/BlockchainWalletPane.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import './BlockchainWalletPane.scss';
+import { RootState } from '../../../state/RootState';
 
 interface BlockchainWalletPaneProps {
   simulationUid: string;
@@ -11,6 +13,11 @@ export const BlockchainWalletPane: React.FC<BlockchainWalletPaneProps> = (
   props
 ) => {
   const { simulationUid, nodeUid } = props;
+
+  const walletData = useSelector(
+    (state: RootState) =>
+      state.simulation[simulationUid].nodeMap[nodeUid].blockchainApp.wallet
+  );
 
   return (
     <div className="comp-blockchain-wallet-pane">
--- frontend/src/state/simulation/data/blockchain/NodeBlockchainAppData.ts
+++ frontend/src/state/simulation/data/blockchain/NodeBlockchainAppData.ts
@@ -1,7 +1,11 @@
+import { BlockchainConfig } from '../../../../common/blockchain/BlockchainConfig';
 import { BlockchainBlockDbData } from './BlockchainBlockDbData';
 import { BlockchainTxDbData } from './BlockchainTxDbData';
+import { BlockchainWalletData } from './BlockchainWalletData';
 
 export interface NodeBlockchainAppData {
   readonly blockDb: BlockchainBlockDbData;
   readonly txDb: BlockchainTxDbData;
+  readonly wallet: BlockchainWalletData;
+  readonly config: BlockchainConfig;
 }
