ccf486a1bba575ae7a451ad195ab9506a16435fc
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2021-03-16 12:38:14 +0300

    more crypto unit tests

--- common/src/crypto/verifySignature.spec.ts
+++ common/src/crypto/verifySignature.spec.ts
@@ -3,16 +3,38 @@ import { createPublicKey } from './createPublicKey';
 import { hashJsonObj } from './hashJsonObj';
 import { createSignature } from './createSignature';
 import { verifySignature } from './verifySignature';
+import { randomBytes } from 'crypto';
+import { hashBuffer } from './hashBuffer';
 
 const plainText = {
   foo: 'lorem ipsum dolor sit amet',
   bar: ['foo', 'bar', 'baz'],
 };
 
-it('accepts correct signature', () => {
+it('accepts correct signature for objects', () => {
   const privateKey = createPrivateKey();
   const publicKey = createPublicKey(privateKey);
   const plainHash = hashJsonObj(plainText);
+  const signature = createSignature(plainHash, privateKey);
+  const verification = verifySignature(signature, plainHash, publicKey);
+
+  expect(verification).toBeTrue();
+});
+
+it('accepts correct signature for strings', () => {
+  const privateKey = createPrivateKey();
+  const publicKey = createPublicKey(privateKey);
+  const plainHash = hashJsonObj('foobar');
+  const signature = createSignature(plainHash, privateKey);
+  const verification = verifySignature(signature, plainHash, publicKey);
+
+  expect(verification).toBeTrue();
+});
+
+it('accepts correct signature for buffers', () => {
+  const privateKey = createPrivateKey();
+  const publicKey = createPublicKey(privateKey);
+  const plainHash = hashBuffer(randomBytes(32));
   const signature = createSignature(plainHash, privateKey);
   const verification = verifySignature(signature, plainHash, publicKey);
 
