1bb55403bde60360d67056ba45d42e01cfef4b24
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2021-04-01 00:55:00 +0300

    frontend: wip on tx db pane

--- frontend/src/components/blockchain/BlockchainTxCard/BlockchainTxCard.tsx
+++ frontend/src/components/blockchain/BlockchainTxCard/BlockchainTxCard.tsx
@@ -1,17 +1,44 @@
+import _ from 'lodash';
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 
 import './BlockchainTxCard.scss';
-import { BlockchainTx } from '../../../../../backend/src/common/blockchain/tx/BlockchainTx';
+import { hasValue } from '../../../common/utils/hasValue';
+import { useTxOutputGetter } from '../../../hooks/txGetters/useTxOutputGetter';
+import { useTxGetterEverywhere } from '../../../hooks/txGetters/useTxGetterEverywhere';
+import { BlockchainTx } from '../../../common/blockchain/tx/BlockchainTx';
 import { hashTx } from '../../../common/blockchain/utils/hashTx';
 
 interface BlockchainTxCardProps {
+  simulationUid: string;
+  nodeUid: string;
   tx: BlockchainTx;
 }
 
 export const BlockchainTxCard: React.FC<BlockchainTxCardProps> = (props) => {
-  const { tx } = props;
+  const { simulationUid, nodeUid, tx } = props;
+  const { inputs, outputs } = tx;
+
+  const getTx = useTxGetterEverywhere({ simulationUid, nodeUid });
+  const getOutput = useTxOutputGetter(getTx);
+
   const txHash = hashTx(tx);
+
+  const outputSum = _.sumBy(outputs, (o) => o.value);
+
+  /** NaN if one of the outputs cannot be found. */
+  const inputSum = _.sumBy(inputs, (i) => {
+    if (i.isCoinbase) {
+      return 0;
+    }
+
+    const getResult = getOutput(i.previousOutput);
+    return hasValue(getResult) && hasValue(getResult.output)
+      ? getResult.output.value
+      : Number.NaN;
+  });
+
+  const fee = inputSum - outputSum;
 
   return (
     <div className="comp-blockchain-tx-card">
@@ -19,7 +46,19 @@ export const BlockchainTxCard: React.FC<BlockchainTxCardProps> = (props) => {
         <Card.Header>
           Transaction <code>{txHash}</code>
         </Card.Header>
-        <Card.Body>tx details here</Card.Body>
+        <Card.Body>
+          <Row>
+            <Col>Fee: {fee}</Col>
+          </Row>
+          <Row>
+            <Col lg={6} className="border-lg-right">
+              {inputs.length} Inputs (Total: {inputSum})
+            </Col>
+            <Col lg={6}>
+              {outputs.length} Outputs (Total: {outputSum})
+            </Col>
+          </Row>
+        </Card.Body>
       </Card>
     </div>
   );
--- frontend/src/components/blockchain/BlockchainTxPoolPane/BlockchainTxPoolPane.tsx
+++ frontend/src/components/blockchain/BlockchainTxPoolPane/BlockchainTxPoolPane.tsx
@@ -1,11 +1,9 @@
-import _ from 'lodash';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Card } from 'react-bootstrap';
 
 import './BlockchainTxPoolPane.scss';
 import { RootState } from '../../../state/RootState';
-import { hashTx } from '../../../common/blockchain/utils/hashTx';
 import { BlockchainTxCard } from '../BlockchainTxCard/BlockchainTxCard';
 
 interface BlockchainTxPoolPaneProps {
@@ -33,18 +31,18 @@ export const BlockchainTxPoolPane: React.FC<BlockchainTxPoolPaneProps> = (
   return (
     <div className="comp-blockchain-tx-pool-pane">
       <Card>
-        <Card.Header>Mempool</Card.Header>
+        <Card.Header>Mempool ({mempool.length})</Card.Header>
         <Card.Body>
           {mempool.map((tx) => (
-            <BlockchainTxCard tx={tx} />
+            <BlockchainTxCard {...props} tx={tx} />
           ))}
         </Card.Body>
       </Card>
       <Card className="mt-3">
-        <Card.Header>Orphan Transactions</Card.Header>
+        <Card.Header>Orphan Transactions ({orphanage.length})</Card.Header>
         <Card.Body>
           {orphanage.map((tx) => (
-            <BlockchainTxCard tx={tx} />
+            <BlockchainTxCard {...props} tx={tx} />
           ))}
         </Card.Body>
       </Card>
