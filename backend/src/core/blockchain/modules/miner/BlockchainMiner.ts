e7217cbd5dfd78bb3bb8d0fe39d157ae275cb4a3
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2021-04-11 00:25:47 +0300

    backend: unified own block and foreign block receive procedures

--- backend/src/core/blockchain/modules/miner/BlockchainMiner.ts
+++ backend/src/core/blockchain/modules/miner/BlockchainMiner.ts
@@ -116,7 +116,7 @@ export class BlockchainMiner {
     this.successfulAttempt = null;
   };
 
-  public readonly broadcastMinedBlock = (): void => {
+  public readonly assembleMinedBlock = (): BlockchainBlock => {
     if (this.currentState.state !== 'stopped') {
       throw new Error(
         `Miner expected 'stopepd' state, but it is: ${this.currentState}`
@@ -135,33 +135,16 @@ export class BlockchainMiner {
       );
     }
 
-    const block = this.assembleBlock(
+    return this.assembleBlock(
       this.currentState.task.blockTemplate,
       this.successfulAttempt
     );
 
-    // Pop included txs from mempool
-    this.currentState.task.blockTemplate.includedTxHashes.forEach(
-      this.txDb.removeFromMempool
-    );
-
-    const parent = this.blockDb.getBlockInBlockchain(block.header.previousHash);
-
-    // add to own block db
-    if (hasValue(parent)) {
-      this.blockDb.addToBlockchain(block, parent);
-    } else {
-      console.warn(
-        'miner could not find the parent block in its own database, adding to orphanage. block hash:',
-        block.header.previousHash
-      );
-      this.blockDb.addToOrphanage(block);
-    }
-
-    // broadcast to other nodes
-    this.network.broadcastBlock(block);
-
-    this.dismissStoppedState();
+    // Deprecated: See the comment above the method definition.
+    // this.acceptMinedBlock(
+    //   block,
+    //   this.currentState.task.blockTemplate.includedTxHashes
+    // );
   };
 
   private readonly assembleBlock = (
@@ -275,4 +258,35 @@ export class BlockchainMiner {
 
     console.log('miner stopped: ', report);
   };
+
+  // Previously, I wanted to implement it such that the nodes accept their own blocks without checks.
+  // However, it proved difficult to implement and started to introduce many code duplications.
+  // Therefore I decided to give that up and make nodes run their own blocks through the same procedure
+  // that foreign blocks go through. Therefore, this method is now obsolete, but I am commenting it out
+  // instead of removing it; in case we want to return back to this in the future.
+  // ~~ Tarık, 2021-04-10 23:57
+  //
+  // private readonly acceptMinedBlock = (
+  //   block: BlockchainBlock,
+  //   includedTxHashes: string[]
+  // ) => {
+  //   // Pop included txs from mempool
+  //   includedTxHashes.forEach(this.txDb.removeFromMempool);
+  //
+  //   const parent = this.blockDb.getBlockInBlockchain(block.header.previousHash);
+  //
+  //   // add to own block db
+  //   if (hasValue(parent)) {
+  //     this.blockDb.addToBlockchain(block, parent);
+  //   } else {
+  //     console.warn(
+  //       'miner could not find the parent block in its own database, adding to orphanage. block hash:',
+  //       block.header.previousHash
+  //     );
+  //     this.blockDb.addToOrphanage(block);
+  //   }
+  //
+  //   // broadcast to other nodes
+  //   this.network.broadcastBlock(block);
+  // };
 }
