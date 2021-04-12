57db974e323a0222b07283a665a383c545b6dbfa
sukrukirman <sukru.kirman@ug.bilkent.edu.tr>
2021-04-12 15:50:20 +0300

    BlockModal pagination added

--- frontend/src/components/DataExplorerBlockModal/DataExplorerBlockModal.tsx
+++ frontend/src/components/DataExplorerBlockModal/DataExplorerBlockModal.tsx
@@ -6,7 +6,7 @@ import { Block, fetchSingleBlockWithHeight } from '../../apis/SingleBlockAPI';
 import LoaderMask from '../LoaderMask/LoaderMask';
 import DataExplorerBlockDetailTable from '../DataExplorerBlockDetailTable/DataExplorerBlockDetailTable';
 import DataExplorerBlockTransactionCard from '../DataExplorerBlockTransactionCard/DataExplorerBlockTransactionCard';
-
+import Pagination from '@material-ui/lab/Pagination';
 interface DataExplorerBlockModalProps {
   closeHandler: () => void;
   blockHeight: number | null;
@@ -18,6 +18,7 @@ const DataExplorerBlockModal: React.FC<DataExplorerBlockModalProps> = (
   const { closeHandler, blockHeight } = props;
   const [data, setData] = useState<Block | null>(null);
   const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [activePage, setActivePage] = useState<number>(1);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,6 +26,7 @@ const DataExplorerBlockModal: React.FC<DataExplorerBlockModalProps> = (
         setIsFetching(true);
         const block = await fetchSingleBlockWithHeight(blockHeight);
         setData(block);
+        setActivePage(1);
         setIsFetching(false);
       } catch (e) {
         setIsFetching(false);
@@ -33,6 +35,7 @@ const DataExplorerBlockModal: React.FC<DataExplorerBlockModalProps> = (
     };
     fetchData();
   }, [blockHeight]);
+
   return (
     <Modal
       show={blockHeight ? true : false}
@@ -52,15 +55,43 @@ const DataExplorerBlockModal: React.FC<DataExplorerBlockModalProps> = (
           <div>
             <DataExplorerBlockDetailTable data={data} />
             <div className="h4">Block Transactions</div>
+            <div className="row d-flex justify-content-center">
+              <Pagination
+                count={Math.ceil((data?.tx ? data.tx.length : 0) / 10)}
+                color="primary"
+                variant="outlined"
+                size="large"
+                page={activePage}
+                onChange={(e, value) => setActivePage(value)}
+                showFirstButton
+                showLastButton
+              />
+            </div>
+
             <Table hover borderless>
               <tbody>
-                {data.tx.slice(0, 10).map((tx) => (
-                  <tr key={tx.hash}>
-                    <DataExplorerBlockTransactionCard tx={tx} />
-                  </tr>
-                ))}
+                {data.tx
+                  .slice((activePage - 1) * 10, (activePage - 1) * 10 + 9)
+                  .map((tx) => (
+                    <tr key={tx.hash}>
+                      <DataExplorerBlockTransactionCard tx={tx} />
+                    </tr>
+                  ))}
               </tbody>
             </Table>
+
+            <div className="row d-flex justify-content-center">
+              <Pagination
+                count={Math.ceil((data?.tx ? data.tx.length : 0) / 10)}
+                color="primary"
+                variant="outlined"
+                size="large"
+                page={activePage}
+                onChange={(e, value) => setActivePage(value)}
+                showFirstButton
+                showLastButton
+              />
+            </div>
           </div>
         ) : (
           <div>Data couldnt downloaded</div>
