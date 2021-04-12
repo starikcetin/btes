bbc9bff24dfcf9b28381bb66046fd72c5b602ce7
sukrukirman <sukru.kirman@ug.bilkent.edu.tr>
2021-04-12 10:18:24 +0300

    LoaderMask.tsx updated

--- frontend/src/components/LoaderMask/LoaderMask.tsx
+++ frontend/src/components/LoaderMask/LoaderMask.tsx
@@ -1,18 +1,14 @@
 import * as React from 'react';
-
+import loaderIcon from './three-dots.svg';
 interface IProps {}
 
 const LoaderMask: React.FunctionComponent<IProps> = () => (
-  <div className="progress">
-    <div
-      className="progress-bar progress-bar-striped progress-bar-animated h-100"
-      role="progressbar"
-      aria-valuenow={75}
-      aria-valuemin={0}
-      aria-valuemax={100}
-      style={{ width: '75%' }}
-    >
-      Loading
+  <div className="container">
+    <div className="row d-flex justify-content-center align-items-center mt-4">
+      <img src={loaderIcon} className="loader-mask d-block" alt="loaderMask" />
+    </div>
+    <div className="row d-flex justify-content-center align-items-center m-5">
+      <span className="text-secondary h5">Loading...</span>
     </div>
   </div>
 );
