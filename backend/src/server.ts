ece520ffd5444679e01dd1a06aac7db77e8f0c95
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2021-04-18 14:43:16 +0300

    backend: connecting to db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,10 +6,12 @@ dotenv.config();
 import { app } from './app';
 import { socketManager } from './socketManager';
 import { fatalAssert } from './common/utils/fatalAssert';
+import { connectToDb } from './database/connectToDb';
 
 // Get ports to use from environment parameters
 const restPort = process.env.REST_PORT || 3001;
 const socketPort = process.env.SOCKET_PORT || 3002;
+const dbUri = process.env.DB_URI ?? 'mongodb://localhost:27017/btes';
 
 fatalAssert(
   restPort != socketPort,
@@ -27,3 +29,11 @@ app.listen(restPort, () =>
 socketManager.start(socketPort, () =>
   console.log(`Socket server:\thttp://localhost:${socketPort}`)
 );
+
+// Connect to MongoDB
+connectToDb(dbUri)
+  .then(() => console.log(`Connected to MongoDB at ${dbUri}`))
+  .catch((err) => {
+    console.error(`Could not connect to MongoDB at ${dbUri}`);
+    throw err;
+  });
